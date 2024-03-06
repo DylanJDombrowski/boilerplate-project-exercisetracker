62bee53e9f7dd0f81246a0cbd6c6ef4b83417f2e
Dylan Dombrowski <dyland601@gmail.com>
2024-03-06 19:19:29 +0000

    init commit

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const uri = process.env.MONGO_URI
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+// Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define User and Exercise models
+const userSchema = new mongoose.Schema({
+  username: String,
+  log: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }],
 });
 
+const exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: Date,
+});
 
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Exercise', exerciseSchema);
 
+app.post('/api/users', (req, res) => {
+  // Create a new user
+});
 
+app.get('/api/users', (req, res) => {
+  // Get a list of all users
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+app.post('/api/users/:_id/exercises', (req, res) => {
+  // Add an exercise for a user
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  // Retrieve the exercise log of a user
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
